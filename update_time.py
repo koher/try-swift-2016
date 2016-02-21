97fa8bd7cedd1a34bc382166788f3b3f774f0d2f
Yuta Koshizawa <koher@koherent.org>
2016-02-22 01:33:40 +0900

    Implement update_time.py

---
+++ update_time.py
@@ -0,0 +1,26 @@
+import re
+
+def foo(line):
+    time = None
+    m = re.search(r"^\^ \(([0-9]+):([0-9]+), ([0-9]+):([0-9]+)\)", line)
+    if m:
+        time = int(m.group(3)) * 60 + int(m.group(4))
+    elif re.search(r"^\^ \(", line):
+        print("ERROR: " + line)
+    return time, line
+
+new_lines = []
+with open('try-swift.md') as file:
+    timed_lines = map(foo, file.readlines())
+    current_time = 0
+    for time, line in timed_lines:
+        if time:
+            minutes = current_time / 60
+            seconds = current_time % 60
+            new_lines.append(re.sub(r"^\^ \(([0-9]+):([0-9]+)", "^ (%d:%02d" % (minutes, seconds), line))
+            current_time += time
+        else:
+            new_lines.append(line)
+
+with open('try-swift.md', 'w') as file:
+    file.writelines(new_lines)
